93c37631d384812888e69e5f5b1de103556f383a
Christopher Marzulli <chriszulli15@gmail.com>
2022-10-03 14:25:46 -0400

    commit tree 85f3feb33e7b7f78abf98dcba9d859cd9338875b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
 end
